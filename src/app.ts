0c9dc3b308c5addc2d08dd0a35512bc7ae34d9b0
Michael-Rousseau <michael.rousseau@epita.fr>
2024-04-28 06:14:46 +0200

    init + done coding

--- src/app.ts
+++ src/app.ts
@@ -2,33 +2,47 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import "express-async-errors";
-const app = express();
-import {notFoundMiddleware} from "./middleware/not-found";
-import {errorHandlerMiddleware} from "./middleware/error-handler";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import cors from "cors";
+import { notFoundMiddleware } from "./middleware/not-found";
+import { errorHandlerMiddleware } from "./middleware/error-handler";
 import authRouter from "./routes/auth";
 import jobRouter from "./routes/jobs";
-import {connectDB} from "./db/connect";
-
+import { connectDB } from "./db/connect";
 import {authentificatedUser} from "./middleware/authentification";
 
+const app = express();
+
+app.use(helmet());
+
+app.use(cors());
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+});
+app.use(limiter);
 
 app.use(express.json());
 
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs',authentificatedUser, jobRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", authentificatedUser, jobRouter);
 
 app.use(notFoundMiddleware);
+
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
-const URL = process.env.MONGO_URL || ''
-const start = async () =>{
+const mongoURL = process.env.MONGO_URL || "";
+
+const start = async () => {
     try {
-        await connectDB(URL);
-        app.listen(port, () => console.log(`server is running on port ${port}...`));
-    }catch (err)
-    {
+        await connectDB(mongoURL);
+        app.listen(port, () => console.log(`Server is running on port ${port}...`));
+    } catch (err) {
         console.error(err);
     }
-}
+};
+
 start();
